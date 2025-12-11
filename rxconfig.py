3b93c03d121ef84661461a0ba7e68961f9b3b361
Abdullah Mahmood <abdullah.au@outlook.com>
2025-12-11 20:32:18 +0400

    clean up

--- rxconfig.py
+++ rxconfig.py
@@ -1,14 +1,14 @@
 import reflex as rx
 
 config = rx.Config(
-    app_name="Koromi",
+    app_name="koromi",
     frontend_port=3001,
     backend_port=3003,
-    backend_host= '100.125.140.11',
-    api_url= 'http://100.125.140.11:3003',
+    backend_host="100.125.140.11",
+    api_url="http://100.125.140.11:3003",
     deploy_url="http://100.125.140.11:3001",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
-    ]
+    ],
 )
